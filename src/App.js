46659f46cba50505633d056b7f543dbcf20fd800
walid-lhaila <walidlhaila00@gmail.com>
2024-04-15 12:08:42 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className="text-red-600 text-3xl font-bold text-center flex justify-center items-center h-screen">HELLO </h1>
+    </div>
+  );
+}
+
+export default App;
