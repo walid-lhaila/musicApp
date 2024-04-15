21140892b86cf76fe7516fab5e5b62656aca734d
walid-lhaila <walidlhaila00@gmail.com>
2024-04-15 12:18:09 -0400

    change

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1 className="text-red-600 text-3xl font-bold text-center flex justify-center items-center h-screen">HELLO </h1>
+     <h1 className="text-blue-600 text-3xl font-bold text-center flex justify-center items-center h-screen">HELLO </h1>
     </div>
   );
 }
